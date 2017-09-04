96af75357b144eebf21a77f27a080652fef59544
genrus <31629088+genrus@users.noreply.github.com>
2017-09-04 19:27:18 +0200

    Created new file
    Just an example

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+//This is a simple program to test the creation of a file with github.
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::cin.get();
+    return 0;
+}
